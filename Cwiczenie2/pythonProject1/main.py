6088e98a0d43ae099db29e9c5a14d25360be7e99
= <dominiksobanski2901@gmail.com>
2025-04-07 17:18:45 +0200

    25_04_07 2

--- Cwiczenie2/pythonProject1/main.py
+++ Cwiczenie2/pythonProject1/main.py
@@ -22,3 +22,53 @@ print(np.min(b2, axis=1))
 #zadanie 3
 print (15 * a)
 print (10 * b)
+
+#zadanie 4
+a4 = np.array([-10,20,-30])
+b4 = np.array([1,2,3])
+np.transpose(b4)
+print(a4*b4)
+
+#zadanie 5
+a5 = np.array([1,2,3,4,5,6]).reshape(2,3)
+asin = np.sin(a5)
+print(asin)
+
+#zadanie 6
+a6 = np.array([1,2,3,4,5,6]).reshape(2,3)
+acos = np.cos(a6)
+print(acos)
+
+#zadanie 7
+def dodanie(a,b):
+    return (a+b)
+print(dodanie(a5,a6))
+
+#zadanie 8
+a8 = np.arange(9).reshape(3,3)
+for i in np.nditer(a8):
+    print(a8.item(i))
+
+#zadanie 9
+a9 = np.arange(9).reshape(3,3)
+print(np.ndarray.flatten(a9))
+flat = np.ndarray.flatten(a9)
+for i in np.nditer(flat):
+    print(flat[i])
+
+#zadanie 10
+a9 = np.arange(81).reshape(9,9)
+print(a9)
+print(a9.reshape(3,27))
+print(a9.reshape(1,81))
+
+#zadanie 11
+
+
+
+
+
+
+
+
+
